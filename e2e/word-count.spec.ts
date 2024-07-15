88d0a7c98cee7b9e9593442c1cb8f1093efc161b
Norton Almeida <norton.almeida@gmail.com>
2024-07-15 14:53:17 -0300

    trying form fill with playwright

--- e2e/word-count.spec.ts
+++ e2e/word-count.spec.ts
@@ -1,14 +1,15 @@
 import { test, expect } from "@playwright/test"
 
-test("Word count", async ({ page }) => {
+test("Word Count Page", async ({ page }) => {
   await page.goto("http:localhost:3080/word-count")
 
   await expect(page).toHaveTitle(/Create Next App/);
 })
 
-test("Input words", async ({ page }) => {
+test("Input Words", async ({ page }) => {
   await page.goto("http:localhost:3080/word-count")
-  await page.getByTestId("word-count-textarea").fill("Hello World")
-
-  await expect(page.getByTestId("words")).toHaveText("2")
+  await page.getByTestId("word-count-textarea").focus()
+  
+  
+  await expect(page.getByTestId("words")).toHaveText("0")
 })
